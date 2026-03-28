32d7d9f83c1898ecd7e90dc3d4aef6be84fce0eb
HQ\AgrawalN <nikhil.agrawal@rotary.org>
2026-03-28 17:00:52 -0500

    list of customers

--- Second_attempt.py
+++ Second_attempt.py
@@ -1,1 +1,24 @@
-print("Hello world!")
+#Create a list of customers
+customers = [
+	"Alice",
+	"Bob",
+	"Charlie",
+	"Diana",
+	"Ethan",
+	"Fiona",
+	"George",
+	"Hannah",
+	"Ian",
+	"Julia",
+	"Kevin",
+	"Laura",
+	"Michael",
+	"Nina",
+	"Oscar",
+	"Paula"
+]
+
+#Print the list of customers
+print("List of customers:")
+for customer in customers:
+    print(customer)
